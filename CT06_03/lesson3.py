84b9e243a99bfa2d6af79e610645a94a3786d920
Emily Wong <emily.wongjq@gmail.com>
2025-01-25 06:22:49 +0000

    Jan 25, 2025, 6:22 AM

--- CT06_03/lesson3.py
+++ CT06_03/lesson3.py
@@ -2,9 +2,11 @@ print("Hello from lesson 3")
 #create variable Adam =60
 #create variable brandon=58
 #create variable charlie=98
-#create variable daniel
+#create variable daniel=79
 #create variable total_score
 #create variable average_score
+
+
 
  
 
