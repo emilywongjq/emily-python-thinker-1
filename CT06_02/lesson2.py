2f23bd89934c71f49f64f558e860c0835782a9d2
Emily Wong <emily.wongjq@gmail.com>
2025-01-25 05:54:59 +0000

    Jan 25, 2025, 5:54 AM

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -1,7 +1,58 @@
-print("Hello from lesson 2")
+#this is for recap
+# print("red")
+# print("orange")
+# print("yellow")
+# print("green")
+# print("blue")
+# print("indigo")
+# print("violet")
+# print("actually i cant decide which one to pick")
 
-######## Write the pseudocode in comments for task 2 here
-# Using comments, translate the code shown on screen into pseudocode.
+#this is the output of the qn/ ans
+#if the age is under the minimum age, the console will print access denied unless the age of the person is higher than the minimum age then the console will print out welcome
 
-######## Write the pseudocode in comments for task 3 here
-# Using comments, translate the code shown on screen into pseudocode.
+#repeat 10 times
+#   say "hey!"
+#   move 10 steps to the right
+
+#set the variable "counter"
+#while variable "counter"is not 50
+#   move 10 steps forward
+#   turn 15 degrees
+#   increase counter by +10
+
+#print=input("what is your age?")
+#   if input less than 18
+        #say access denied
+#   else
+        #say welcome
+
+
+#while there
+#ask("is this plastic")
+    #if answer="no"
+        #ask("is this paper")
+            #if answer="no"
+                #place in glass bin
+            #else place in paper bin
+    #else place in plastic bin
+
+
+####################################################################secret#phrase#qn###############################################################################################
+
+#set variable "password" to ______
+#ask "whats the password"
+    # if ans not = "password"
+        #say "you are fired for either your bad memory or you are a scammer"
+    #else say "congrats, welcome!"
+
+##################################################################student#scor#qn##################################################################################################
+#create the variable test1
+#create the variable test2
+#create the variable test3
+#create the variable totalScore
+#totalScore = test1 x 20% + test2 x 40% + test3 x 40%
+
+# Return totalScore to the userrrrrrrrrr
+
+###########################################################################the#end#################################################################################################
