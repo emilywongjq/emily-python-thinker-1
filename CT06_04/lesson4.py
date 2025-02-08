5b6de59eb01b695d637ec6839b4cef83dc4473bc
Emily Wong <emily.wongjq@gmail.com>
2025-02-08 07:27:05 +0000

    Feb 8, 2025, 7:27 AM

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -30,7 +30,7 @@ print("my comment🤣is: "+ comment)
 
 
 name = input("What is ur name?")
-print("hi, "+ name )
+print("hi😸, "+ name )
 
 hobby = input("What is your favourite hobby?")
 print("I enjoy🥎🏀🏐🏈🎱🤿 "+ hobby)
