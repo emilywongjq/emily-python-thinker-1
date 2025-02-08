0d6f8ce1a620c6ecc17b8337a6427b623b4f8df2
Emily Wong <emily.wongjq@gmail.com>
2025-02-08 06:54:31 +0000

    Feb 8, 2025, 6:54 AM

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -1,13 +1,13 @@
 # print("Hello from lesson 4")
 # print("レッスン 4 からこんにちは")
 
-red_price = 1
-blue_price = 2
-green_price = 3
+# red_price = 1
+# blue_price = 2
+# green_price = 3
 
-red_plates = 3
-blue_plates = 5
-green_plates = 4
+# red_plates = 3
+# blue_plates = 5
+# green_plates = 4
 
-total_amt = (red_plates*red_price)+(blue_price*blue_plates)+(green_plates*green_price)
-print(total_amt)
+# total_amt = (red_plates*red_price)+(blue_price*blue_plates)+(green_plates*green_price)
+# print(total_amt)
