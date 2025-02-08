d4d818350a95e40429c8c0f239a6a81bf960c54b
Emily Wong <emily.wongjq@gmail.com>
2025-02-08 06:49:14 +0000

    Feb 8, 2025, 6:49 AM

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -9,5 +9,5 @@ red_plates = 3
 blue_plates = 5
 green_plates = 4
 
-total_amt = (red_plates*red_plates)+(blue_price*blue_plates)+(green_plates*green_price)
+total_amt = (red_plates*red_price)+(blue_price*blue_plates)+(green_plates*green_price)
 print(total_amt)
