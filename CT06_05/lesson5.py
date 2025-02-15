ba0a33e3ca3912ecd01906e1543cecc2d865dbf8
Emily Wong <emily.wongjq@gmail.com>
2025-02-15 06:28:36 +0000

    Feb 15, 2025, 6:28 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -8,7 +8,7 @@ print("Next year, you would be "+ str(age)+" years old.")
 
 number = input("Pick a number from 1-1000")
 double = number*2
-print("Double your number is "+ str(double) +" .")
+print("Double your number is "+ str(double))
 
 year = input("What year were you born in?")
 current_year = 2025
