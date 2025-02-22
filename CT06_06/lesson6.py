0b4979b314e8bd3cd2ab9c573d291d641cf1430d
Emily Wong <emily.wongjq@gmail.com>
2025-02-22 06:57:32 +0000

    Feb 22, 2025, 6:57 AM

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -5,6 +5,6 @@ students = int(input("How many students are there?"))
 score = 0
 
 for i in range(students):
-    score = score + int(input("What is the score of this pupil?"))
+    score = score + int(input("What is the score of this pupil?╰(*°▽°*)╯(☆▽☆)"))
 
 print(score/students)
