b4d2f43db9844434c1e76cffaccf97e63a9cb6c0
Emily Wong <emily.wongjq@gmail.com>
2025-02-22 06:23:37 +0000

    Feb 22, 2025, 6:23 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -29,10 +29,10 @@
 #     print(z)
 # for x in range(18,30):
 #     print(x)
-#######task#5b#########
+#######task#5a#########
 for e in range(2,26,2):
     print(e)
-#######task#5b
+#######task#5b########
 for i in range(8,97,8):
     print(i)
 
