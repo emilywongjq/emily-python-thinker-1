c5197da893c092c6f100f2d4b2714cadc262e47d
Emily Wong <emily.wongjq@gmail.com>
2025-02-22 06:58:17 +0000

    Feb 22, 2025, 6:58 AM

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -1,10 +1,13 @@
 # print("Hello from lesson 6")
 # print("レッスン6からこんにちは")
 
-students = int(input("How many students are there?"))
+students = int(input("How many students are there?opa                     ╱|、
+                          (˚ˎ 。7  
+                           |、˜〵          
+                          じしˍ,)ノ"))
 score = 0
 
 for i in range(students):
-    score = score + int(input("What is the score of this pupil?╰(*°▽°*)╯(☆▽☆)"))
+    score = score + int(input("What is the score of this pupil?(☆▽☆)"))
 
 print(score/students)
