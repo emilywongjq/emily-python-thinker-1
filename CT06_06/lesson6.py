f41d0497ae7e0c77b7381b0963112522b67ba9ae
Emily Wong <emily.wongjq@gmail.com>
2025-02-22 07:09:50 +0000

    Feb 22, 2025, 7:09 AM

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -1,10 +1,31 @@
 # print("Hello from lesson 6")
 # print("レッスン6からこんにちは")
 
-students = int(input("How many students are there?"))
-score = 0
+# students = int(input("How many students are there?"))
+# score = 0
 
-for i in range(students):
-    score = score + int(input("What is the score of this pupil?(☆▽☆)"))
+# for i in range(students):
+#     score = score + int(input("What is the score of this pupil?(☆▽☆)"))
 
-print(score/students)
+# print(score/students)
+
+
+## Task 1: Syntax Errors
+# Fix the errors in the following:
+
+# **Task 1a**:
+# for i in range(3)
+#     print("Hello, World!")
+
+# **Task 1b**:
+# for i in range(5):
+# print(i)
+
+# **Task 1c**:
+# print("Hello, World!)
+
+# **Task 1d**:
+# if = 5
+
+# **Task 1e**:
+# print "Hello, World!"
