d1d1c6bd1eb9f0646641cead209faa8f00b70446
Emily Wong <emily.wongjq@gmail.com>
2025-03-01 05:53:08 +0000

    Mar 1, 2025, 5:53 AM

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -9,23 +9,70 @@
 
 # print(score/students)
 
-# ________________________
+# ________________________( -_•)╦̵̵̿╤─  (ง •̀⤙•́ )ง________________________
 ## Task 1: Syntax Errors
 # Fix the errors in the following:
-
 # **Task 1a**:
-# for i in range(3)
-#     print("Hello, World!")
+for i in range(3)
+    print("Hello, World!")
 
 # **Task 1b**:
-# for i in range(5):
-# print(i)
+for i in range(5):
+    print(i)
 
 # **Task 1c**:
-# print("Hello, World!)
+print("Hello, World!")
 
 # **Task 1d**:
-# if = 5
+fi = 5
 
 # **Task 1e**:
-# print "Hello, World!"
+print ("Hello, World!")
+#________________________________________________________________________
+## Task 2: Name Errors
+#Fix the errors in the following:
+
+#**Task 2a**:
+age = input("What is your age?")
+print(age)
+
+#**Task 2b**:
+name = "Alice"
+print(name)
+
+#**Task 2c**:
+x = 5
+print(x)
+
+#**Task 2d**:
+print("Hello, World!")
+
+#___________________*ੈ✩‧₊˚ (p= w =p▄︻デ══━一___________________________________
+## Task 3: Type Errors
+# Fix the errors in the following:
+
+# **Task 3a**:
+age = 25
+print(age + 1)
+
+#**Task 3b**:
+number = 10
+print(number - 5)
+
+#**Task 3c**:
+print("Repeat" * 3)
+
+#**Task 3d**:
+year = 2023
+print("The year is " + str(year))
+
+#**Task 3e**:
+x = 10
+y = x / 2
+
+#**Task 3f**:
+end = 5
+for i in range(end):
+    print(i)
+
+
