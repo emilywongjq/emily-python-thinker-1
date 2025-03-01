7e4838dccd2022e12cb38157bf8e6d03d07d0657
Emily Wong <emily.wongjq@gmail.com>
2025-03-01 06:40:05 +0000

    Mar 1, 2025, 6:40 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -1,14 +1,16 @@
 # print("Hello from lesson 7")
 # print("レッスン 7 からこんにちは")
 #________________________________________________________________________
-score_one = 80
-score_two = 90
-score_three = 75
+# score_one = 80
+# score_two = 90
+# score_three = 75
 
-total = score_one + score_two + score_three
+# total = score_one + score_two + score_three
 
-average_score = str(total / 3)
+# average_score = str(total / 3)
 
-student_name = "Alex"
+# student_name = "Alex"
 
-print ("Average score for " + student_name + " is: " + average_score)
+# print ("Average score for " + student_name + " is: " + average_score)
+
+#________________________________________________________________________
