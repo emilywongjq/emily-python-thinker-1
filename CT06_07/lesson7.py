68d4845b5e1403bcc5229d02c89cb6b8fc3ace11
Emily Wong <emily.wongjq@gmail.com>
2025-03-01 06:30:30 +0000

    Mar 1, 2025, 6:30 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -9,6 +9,6 @@ total = score_one + score_two + score_three
 
 average_score = total / 3
 
-student_name = str("Alex")
+student_name = "Alex"
 
 print("Average score for " + student_name + " is: " + average_score)
