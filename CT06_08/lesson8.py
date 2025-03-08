6b3a396259845d91ac46f9e5e805f6d58f13b8c3
Emily Wong <emily.wongjq@gmail.com>
2025-03-08 06:25:08 +0000

    Mar 8, 2025, 6:25 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -2,4 +2,5 @@ name = input("What is your name?")# asking user for their name
 print("Nice to meet you "+ name)# print out the sentence
 
 
-start_number = int(in)
+start_number = int(input("What number do you want to start with?"))
+stop_
