d6b26b95da9d7d17ac06adf816519f5e468d9b35
Emily Wong <emily.wongjq@gmail.com>
2025-03-08 06:27:35 +0000

    Mar 8, 2025, 6:27 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -4,4 +4,5 @@ print("Nice to meet you "+ name)# print out the sentence
 
 start_number = int(input("What number do you want to start with?"))# ask the user what number do they want to start with
 stop_number = int(input("What number do you want to stop at?"))# ask the user what number do they want to stop at
-step_number = int(input("What is your step?"))# ask the user what is their step(the differe)
+step_number = int(input("What is your step?"))# ask the user what is their step(the difference between 2 numbers)
+
