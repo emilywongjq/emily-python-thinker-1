31973ce3da221e84d4cfe7055b5640954ff6274a
Emily Wong <emily.wongjq@gmail.com>
2025-03-29 07:16:45 +0000

    Mar 29, 2025, 7:16 AM

--- CT06_10/lesson10.py
+++ CT06_10/lesson10.py
@@ -27,9 +27,9 @@
 score = int(input("What is your score"))
 if score == 100 :
     print("A+")
-elif score > 90 :
+elif score >= 90 :
     print("A")
-elif score > 80 :
+elif score >= 80 :
     print("B")
 elif score > 70 :
     print("C")
