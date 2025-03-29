abeae7020b2279653446bdd24c6746dacfe838b6
Emily Wong <emily.wongjq@gmail.com>
2025-03-29 06:57:44 +0000

    Mar 29, 2025, 6:57 AM

--- CT06_10/lesson10.py
+++ CT06_10/lesson10.py
@@ -1,10 +1,12 @@
 # print("Hello from lesson 10")
 
-num = int(input("Guess a number...."))
-if num > 0:
-    print("positive")
-else:
-    print("ur not an alien")
-    print("ERROR")
-    print("Negative")
-    print("Conclusion: You are an alien")
+# num = int(input("Guess a number...."))
+# if num > 0:
+#     print("positive")
+# else:
+#     print("ur not an alien")
+#     print("ERROR")
+#     print("Negative")
+#     print("Conclusion: You are an alien")
+
+num = 
