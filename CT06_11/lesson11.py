0e37119518975506d15e841b6c514ee1c2e0f877
Emily Wong <emily.wongjq@gmail.com>
2025-04-06 03:19:54 +0000

    Apr 6, 2025, 3:19 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -1,10 +1,11 @@
 
-px = int(input("What is the price of the item?"))
-if px <= 5:
-    print("Sounds good!")
-elif px <= 50:
-    print("Are you sure you need this?")
-elif px <= 500:
-    print("Where are you getting the money to buy this?")
-else: # else (for else, don't need condition)
-    print("Don't even think about buying that")
+# px = int(input("What is the price of the item?"))
+# if px <= 5:
+#     print("Sounds good!")
+# elif px <= 50:
+#     print("Are you sure you need this?")
+# elif px <= 500:
+#     print("Where are you getting the money to buy this?")
+# else: # else (for else, don't need condition)
+#     print("Don't even think about buying that")
+
