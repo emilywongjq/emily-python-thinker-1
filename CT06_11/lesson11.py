57ca4a53ebcad1bfe9bcedc8b9ecb1d73f8ae15f
Emily Wong <emily.wongjq@gmail.com>
2025-04-06 03:14:12 +0000

    Apr 6, 2025, 3:14 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -6,5 +6,5 @@ elif px <= 50:
     print("Are you sure you need this?")
 elif px <= 500:
     print("Where are you getting the money to buy this?")
-else: # else
-    print("Don't ")
+else: # else (for else, don't need condition)
+    print("Don't even think about buying that")
