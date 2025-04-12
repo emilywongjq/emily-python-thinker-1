c368dd93daa20d2f8a4b2e1926f7e5f8d3c73d60
Emily Wong <emily.wongjq@gmail.com>
2025-04-12 06:17:31 +0000

    Apr 12, 2025, 6:17 AM

--- CT06_12/lesson12.py
+++ CT06_12/lesson12.py
@@ -1,8 +1,9 @@
 # print("Hello from lesson 12")
 
-num = input("What is a random number that you can think of? :")
+# num = input("What is a random number that you can think of? :")
 
-if num % 3 == 0 and num % 5 == 0:
-    print("The number is divisible by 3 abd 5")
-else:
-    print("The number is not divisible by 3 abd 5")
+# if num % 3 == 0 and num % 5 == 0:
+#     print("The number is divisible by 3 abd 5")
+# else:
+#     print("The number is not divisible by 3 abd 5")
+
