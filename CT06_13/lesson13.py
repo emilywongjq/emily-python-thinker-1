8c02920989a1a215fb4cbc5304be06753b3948ab
Emily Wong <emily.wongjq@gmail.com>
2025-04-19 06:31:10 +0000

    Apr 19, 2025, 6:31 AM

--- CT06_13/lesson13.py
+++ CT06_13/lesson13.py
@@ -13,7 +13,7 @@ while True:
             balance -= amount
             print(balance)
         else:
-            print("Error://!?U are too broke to afford this transfer of ")
+            print("Error://!?U are too broke.")
     if userChoice == 2:
         amount = int(input("How much do you want to deposit?"))
         balance += amount
