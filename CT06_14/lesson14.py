1ad7393ee2f0669e61e7b395804972b0032b2522
Emily Wong <emily.wongjq@gmail.com>
2025-04-26 07:27:56 +0000

    Apr 26, 2025, 7:27 AM

--- CT06_14/lesson14.py
+++ CT06_14/lesson14.py
@@ -6,9 +6,9 @@ t.shape('turtle')
 t.fillcolor('green')
 
 t.seth(0)
-for i in range(360):
+for i in range(5):
     t.pendown()
-    t.left(1)
+    t.left(72)
     t.forward(1)
 t.pendown()
 window.mainloop()
