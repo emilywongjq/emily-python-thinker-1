4f9a79d1afec50fe11afede6158a0ccb794a9e8c
Emily Wong <emily.wongjq@gmail.com>
2025-05-10 06:38:33 +0000

    May 10, 2025, 6:38 AM

--- CT06_14/test.py
+++ CT06_14/test.py
@@ -1,4 +1,11 @@
 #Task 1
+# Print numbers from 10 to 200 using the while loop
+# Your numbers must be in multiples of 10.
+# 10 must be first number printed, and 200 the last number.
+
+# Example: 10, 20, 30 ..... 180, 190, 200.
+# Note that the numbers do not need to be printed in one line.
+# Write your code here:
 number = 10 #set the number to 10
 while number <= 200 :
     print(number)
