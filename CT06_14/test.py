edeb3e0562525342374891822ceb560f4f9295b2
Emily Wong <emily.wongjq@gmail.com>
2025-05-10 06:46:25 +0000

    May 10, 2025, 6:46 AM

--- CT06_14/test.py
+++ CT06_14/test.py
@@ -57,7 +57,7 @@ planets = [
 print(planets[2])
 
 # 2. Write code to append neptune to this list.
-
+planets.append
 
 # 3. Elon Musk has conquered Mars. 
 #    Rename Mars in the list to be "muskworld"
