6a726b38c5179feb52f5371a38994d6a72a0d8b0
Emily Wong <emily.wongjq@gmail.com>
2025-05-10 07:00:06 +0000

    May 10, 2025, 7:00 AM

--- CT06_14/test.py
+++ CT06_14/test.py
@@ -43,30 +43,32 @@
 # Do the following operations to the list provided below
 # Write the code below each question.
 
-planets = [
-    "mercury",
-    "venus", 
-    "earth",
-    "mars", 
-    "jupiter", 
-    "saturn", 
-    "uranus" ]
+# planets = [
+#     "mercury",
+#     "venus", 
+#     "earth",
+#     "mars", 
+#     "jupiter", 
+#     "saturn", 
+#     "uranus" ]
 
-# 1. Write code below to print the 3rd item 
-#    in this list using index e.g. earth
-print(planets[2])
+# # 1. Write code below to print the 3rd item 
+# #    in this list using index e.g. earth
+# print(planets[2])
 
-# 2. Write code to append neptune to this list.
-planets.append("neptune")
+# # 2. Write code to append neptune to this list.
+# planets.append("neptune")
 
-# 3. Elon Musk has conquered Mars. 
-#    Rename Mars in the list to be "muskworld"
-planets[3] = "muskworld"
+# # 3. Elon Musk has conquered Mars. 
+# #    Rename Mars in the list to be "muskworld"
+# planets[3] = "muskworld"
 
-# 4. Remove uranus from this list.
-del(planets[6])
+# # 4. Remove uranus from this list.
+# del(planets[6])
 
-# 5. Using a for loop, print all the planets 
-#    from this list one by one.
-for item in planets:
-    print(item)
+# # 5. Using a for loop, print all the planets 
+# #    from this list one by one.
+# for item in planets:
+#     print(item)
+
+
