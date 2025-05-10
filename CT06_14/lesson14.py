a0f35f627642db3fcb72736b78149ce83c40edd7
Emily Wong <emily.wongjq@gmail.com>
2025-05-10 06:04:06 +0000

    May 10, 2025, 6:04 AM

--- CT06_14/lesson14.py
+++ CT06_14/lesson14.py
@@ -1,14 +1,14 @@
-import turtle 
-window = turtle.Screen()
-window.setup(width= 600, height= 400)
-t = turtle.Turtle()
-t.shape('turtle')
-t.fillcolor('green')
+# import turtle 
+# window = turtle.Screen()
+# window.setup(width= 600, height= 400)
+# t = turtle.Turtle()
+# t.shape('turtle')
+# t.fillcolor('green')
 
-t.seth(0)
-for i in range(5):
-    t.pendown()
-    t.left(72)
-    t.forward(50)
-t.pendown()
-window.mainloop()
+# t.seth(0)
+# for i in range(5):
+#     t.pendown()
+#     t.left(72)
+#     t.forward(50)
+# t.pendown()
+# window.mainloop()
