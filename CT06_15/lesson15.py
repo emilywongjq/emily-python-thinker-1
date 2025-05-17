f976920752f7cd00509549d96a7be1b4f8215387
Emily Wong <emily.wongjq@gmail.com>
2025-05-17 06:40:35 +0000

    May 17, 2025, 6:40 AM

--- CT06_15/lesson15.py
+++ CT06_15/lesson15.py
@@ -1,11 +1,24 @@
-counter = 0
+# # Task 3: Increment Counter
+# Create a function that will increase the 'counter' variable by 1 each
+# time it is called.
 
-def increment_counter ():
-    global counter
-    counter += 1
+# 1. Create a 'counter' variable and assign it '0'
+# 2. Define a function 'increment_counter()':
+#         a. Declare 'counter' as 'global'
+#         b. Add 1 to 'counter'
+# 3. Test your program out by calling the 'increment_counter()' function
+# 4. 3 times before printing out the value of the 'counter' variable.
 
-increment_counter()
-increment_counter()
-increment_counter()
+# Your output should be "3"
 
-print (counter)
+# counter = 0
+
+# def increment_counter ():
+#     global counter
+#     counter += 1
+
+# increment_counter()
+# increment_counter()
+# increment_counter()
+
+# print (counter)
