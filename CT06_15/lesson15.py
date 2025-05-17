f349641cf5b75dda5952813c7fe8128721324e77
Emily Wong <emily.wongjq@gmail.com>
2025-05-17 07:12:00 +0000

    May 17, 2025, 7:12 AM

--- CT06_15/lesson15.py
+++ CT06_15/lesson15.py
@@ -69,7 +69,7 @@
 def doubleNumber(num):
     for n in num:
         num1 = n * 2
-        print('Double of ' + str)
+        print('Double of ' + str('n') + '')
 # ---------------------------------------------------------------------
 
 # # Task 6: Greetings III
